b82f3e70a51f4e6f01a700a02633bb39b85b044e
Martin Zabransky <investradarsk@gmail.com>
2026-09-19 09:21:32 +0200

    ARTICLE-EDITOR-UX-FIX: add nullable hero image metadata

---
+++ drizzle/0046_article_hero_image_metadata.sql
@@ -0,0 +1,4 @@
+ALTER TABLE managed_articles ADD COLUMN image_alt TEXT;
+ALTER TABLE managed_articles ADD COLUMN image_caption TEXT;
+ALTER TABLE managed_articles ADD COLUMN image_credit TEXT;
+ALTER TABLE managed_articles ADD COLUMN image_credit_url TEXT;
