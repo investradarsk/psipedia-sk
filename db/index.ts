a33809f779221a682fdce33d74297b6e6a5b399f
Martin Zabransky <investradarsk@gmail.com>
2026-09-21 23:22:47 +0200

    PARTNER-1B: register partner schema

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,9 @@
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/d1";
-import * as schema from "./schema";
+import * as coreSchema from "./schema";
+import * as partnerSchema from "./partner-schema";
+
+const schema = { ...coreSchema, ...partnerSchema };
 
 export function getDb() {
   if (!env.DB) {
