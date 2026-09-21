f429d7105c9f7f85085ce1cb6483f3aec7da28fc
Martin Zabransky <investradarsk@gmail.com>
2026-09-21 13:01:19 +0200

    Mock Cloudflare waitUntil in tests

--- tests/cloudflare-loader.mjs
+++ tests/cloudflare-loader.mjs
@@ -1,5 +1,9 @@
 const cloudflareWorkersModule = `
   export const env = (globalThis.__CLOUDFLARE_WORKERS_ENV__ ??= {});
+  export const waitUntil = (promise) => {
+    globalThis.__CLOUDFLARE_WAIT_UNTIL__ = promise;
+    promise.catch(() => undefined);
+  };
 `;
 
 export async function resolve(specifier, context, nextResolve) {
